eb210560c1de1c2b73e8f58c9e85d4c422e988c2
mburgomaes <mburgomaes@gmail.com>
2019-02-12 10:48:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tema7cardlist/App.java
@@ -0,0 +1,16 @@
+package com.example.tema7cardlist;
+
+import com.example.tema7cardlist.Model.Producto;
+import com.example.tema7cardlist.Model.Producto;
+
+public class App {
+
+    public final static int INSERTAR = 1;
+    public final static int EDITAR = 2;
+    public final static int INFORMACION = 3;
+
+    public static int accion;
+
+    public static Producto productoActivo;
+
+}
---
+++ app/src/main/java/com/example/tema7cardlist/edicion.java
@@ -0,0 +1,67 @@
+package com.example.tema7cardlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.tema7cardlist.Logic.LogicProducto;
+import com.example.tema7cardlist.Logic.LogicProducto;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class edicion extends AppCompatActivity {
+
+    private EditText txtNombre;
+    private EditText txtCantidad;
+    private Spinner lstSeccion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edicion);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtCantidad = findViewById(R.id.txtCantidad);
+        lstSeccion = findViewById(R.id.lstSeccion);
+
+        List secciones = Arrays.asList("Monitor", "DiscoDuro", "Memoria", "Teclado", "Ratón", "Impresora");
+        lstSeccion.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, secciones));
+
+        txtNombre.setText(App.productoActivo.getNombre());
+        txtCantidad.setText(App.productoActivo.getCantidad().toString());
+        lstSeccion.setSelection(secciones.indexOf(App.productoActivo.getSeccion()));
+    }
+
+    public void clicGuardar(View view) {
+        String nombre = txtNombre.getText().toString();
+        String cantidad = txtCantidad.getText().toString();
+        String seccion = lstSeccion.getSelectedItem().toString();
+
+        if (nombre.equals("") || cantidad.equals("")) {
+            mostrarMensaje("Faltan datos obligatorios.");
+        } else {
+            App.productoActivo.setNombre(nombre);
+            App.productoActivo.setCantidad(Integer.parseInt(cantidad));
+            App.productoActivo.setSeccion(seccion);
+            switch (App.accion) {
+                case App.INSERTAR:
+                    LogicProducto.insertarProducto(this, App.productoActivo);
+                    break;
+                case App.EDITAR:
+                    LogicProducto.editarProducto(this, App.productoActivo);
+                    break;
+            }
+            mostrarMensaje("Producto " + nombre + " ha sido almacenado.");
+            finish();
+        }
+    }
+
+    private void mostrarMensaje(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+}
